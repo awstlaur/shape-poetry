f94dadb303bb60d4bcc80710d7a9d55d4a99f6dd
shartse <sara.hartse@gmail.com>
2015-02-07 17:25:46 -0500

    writing poem to page based on bitmap

--- parsing.js
+++ parsing.js
@@ -1,13 +1,32 @@
-console.log('hello');
 
-function writePoem(frame) {
-    document.write(frame.name);
-   
+/* writes poem to document based on frame */
+function writePoem(poem, bitmap, fontSize) {
+    poemElement = document.createElement('p');
+    poemElement.innerHTML = "";
+    document.write(poem.name + '</br>');
+    var body = poem.body;
+    for(y = 0; y < bitmap.length; y++) {
+        poemElement.innerHTML += '</br>';
+        for(i = 0; i + fontSize < bitmap[y].length; i += fontSize) {
+            if (body.length >= fontSize) {
+                if (bitmap[y][i] != 0) {
+                    poemElement.innerHTML += body.substring(0, fontSize);
+                    body = body.substring(fontSize);
+                } else {
+                    poemElement.innerHTML += '&nbsp';
+                }
+            } else {
+                window.onload = function() {
+                    document.getElementById('poem').appendChild(poemElement);
+                }
+                return;
+            }
+        }
+    }
+    window.onload = function() {
+        document.getElementById('poem').appendChild(poemElement);
+    }
 }
 
-function makeFrame(poem) {
-    var f = new frame(poem.name, []);
-    return f;
-}
-
-writePoem(makeFrame(new poem('the title', 'the body')));
+var bitmap = [[0,0,1,1,1,0,0,1,1,1], [0,1,1,0,1,1,1], [0,0,0,0,1,1,1,1], [0,1,1,1]]
+writePoem(new poem('the title', 'the body of the poem'), bitmap, 1);
